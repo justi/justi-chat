2167ff21a9fd66423d6a0f033f507b3de726a37e
justi <justyna@galdomedia.pl>
2013-07-27 18:43:50 +0200

    redis test

--- web.js
+++ web.js
@@ -1,5 +1,18 @@
 var express = require("express");
 var app = express();
+var redis = require('redis');
+var url = require('url');
+
+// Configure connection to your Redis Cloud service using REDISCLOUD_URL config vars
+// heroku config:get REDISCLOUD_URL
+// http://rediscloud:password@hostname:port
+// ex. set in .env file:
+// REDISCLOUD_URL=........
+
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
@@ -9,4 +22,11 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+// test redis db
+// REDISCLOUD_URL should be set !!
+client.set('foo', 'bar');
+client.get('foo', function (err, reply) {
+    console.log(reply.toString()); // Will print `bar`
 });
